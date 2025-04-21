7c1207c54db056a04c6f9e3a9601f2902a0703b7
Miguel Garcia <112121391+MiguelGarcia2002@users.noreply.github.com>
2025-04-20 20:20:32 -0400

    added more files for GNN and waht not

--- src/GNN.py
+++ src/GNN.py
@@ -1,29 +1,19 @@
 #imports
 import torch
+import traci
 import torch.nn as nn
 from torch_geometric import data
 import torch.nn.functional as F
 from torch_geometric.nn import GCNConv
 
-data_object = torch.load("../data/data.pt")
-
 class GNN(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim):
         super(GNN, self).__init__()
         self.conv1 = GCNConv(input_dim, hidden_dim)
-        self.conv2 = GCNConv(input_dim, hidden_dim)
+        self.conv2 = GCNConv(hidden_dim, output_dim)
 
     def forward(self, x, edge_index):
         x = self.conv1(x, edge_index)
         x = F.relu(x)
         x = self.conv2(x, edge_index)
         return x
-
-# model params
-input_dim = 4
-hidden_dim = 8
-output_dim = 2
-
-# model = GNN(input_dim, hidden_dim, output_dim)
-
-print(data_object)
